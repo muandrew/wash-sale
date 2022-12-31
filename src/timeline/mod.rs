30f70d5472b282bf4bcbb8f78ed80bd82b49fa20
Andrew Mu <muandrew@gmail.com>
2022-12-30 20:44:46 -0800

    add total_gains and value_per_share to event

--- src/timeline/mod.rs
+++ src/timeline/mod.rs
@@ -5,6 +5,7 @@ pub fn a_fn() {
     println!("test");
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Thou(pub usize);
 
 impl fmt::Display for Thou {
@@ -58,23 +59,56 @@ impl Thou {
 
 // from the perspective of my account
 
-enum EventType {
+pub enum EventType {
     RELEASE,
     SALE,
     PURCHASE,
 }
-struct Event {
-    date: NaiveDate,
+pub struct Event {
     batch_id: String,
+    date: NaiveDate,
     event_type: EventType,
+    market_price: Thou, // used to compute gain / loss
     shares: usize,
-    amount: i64,
-    market_price: i64,
+    transaction_amount: Thou, // how much the transaction was for
 }
 
 impl Event {
-    fn value_per_share_rounded(&self) -> i64 {
-        self.amount / self.market_price;
-        return 1;
+    /// compute how much each share was worth
+    fn value_per_share(&self) -> (Thou, Thou) {
+        (
+            Thou(self.transaction_amount.0 / self.shares),
+            Thou(self.transaction_amount.0 % self.shares),
+        )
+    }
+
+    /// total gains
+    fn total_gains(&self) -> Thou {
+        Thou(self.transaction_amount.0 - (self.market_price.0 * self.shares))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Event, EventType, Thou};
+    use chrono::prelude::*;
+
+    #[test]
+    fn test_value_per_share() {
+        let event = Event {
+            batch_id: "some_id".to_string(),
+            date: NaiveDate::parse_from_str("2015-09-05", "%Y-%m-%d").unwrap(),
+            event_type: EventType::RELEASE,
+            market_price: Thou::from_dollars(1),
+            shares: 5,
+            transaction_amount: Thou::from_dollars(100),
+        };
+
+        let (vps, extra) = event.value_per_share();
+        assert_eq!(vps, Thou::from_dollars(20));
+        assert_eq!(extra.0, 0);
+
+        let gains = event.total_gains();
+        assert_eq!(gains, Thou::from_dollars(95));
     }
 }
